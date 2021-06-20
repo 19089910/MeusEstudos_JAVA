37e839dd083d8d2da98f8d90ff3a0ad7b686f696
Lucas Barbosa Ferreira <55853372+19089910@users.noreply.github.com>
2021-06-20 19:07:46 -0400

    Update testando-entrada.java
    //TIPOS DE SCANNER E SEU USO
    //TEST DO SCAN
    //index/charAt de uma string
    //converter valor em string
    //Poo vs type primitivos; a prova que java nao e so POO

--- testando-entrada.java
+++ testando-entrada.java
@@ -1,52 +1,59 @@
 import java.util.Scanner;//biblioteca escan
+public class estutura {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);//= type variavelName = novo type(entrar no sistema)/ variavelName recebe type(sua funcao)
 
-public class saida {
-     public static void main(String[] args) {
-         String seila;
+        //TIPOS DE SCANNER E SEU USO
+        System.out.print("informe um nome: ");
+        String Onome;
+        Onome = scan.nextLine();
+        /** String stringname = scanner.nextLine();
+         * Double doubleValor = scanner.nextDoubel();
+         * Fluat floatValor = scanner.nextFluat();
+         * Short shortValor = scanner.nextShort();
+         * Integer intValor = scanner.nextInt();
+         * Byte byteValor = scanner.nextByte();
+         * Boolean bool = scanner.nextBoolean();
+         */
+        System.out.println(Onome);
+        System.out.println("\n FIM DO PROGRMA\n");
 
-         Scanner scan = new Scanner(System.in);//= type variavelName = novo type(entrar no sistema)/ variavelName recebe type(sua funcao)
+        //TEST DO SCAN
+        System.out.print("digite um valor que sera somado com 10: ");
+        Integer digitaAi = scan.nextInt();
+        System.out.println(digitaAi + 10);
+        System.out.println("\nFIM DO PROGRMA\n");
 
-         System.out.print("informe um nome: ");
-         seila = scan.nextLine();/** String stringname = scanner.nextLine();
-                                   * Double doubleValor = scanner.nextDoubel();
-                                   * Fluat floatValor = scanner.nextFluat();
-                                   * Short shortValor = scanner.nextShort();
-                                   * Integer intValor = scanner.nextInt();
-                                   * Byte byteValor = scanner.nextByte();
-                                   * Boolean bool = scanner.nextBoolean();
-                                */
-         System.out.println(seila);
-        //teste
-         System.out.print("digite um valor: ");
-         Integer digitaAi = scan.nextInt();
-         System.out.println(digitaAi + 10);
+        //index/charAt de uma string
+        String nome = "lucas";
+        System.out.println(nome.charAt(0) + "-" + nome.charAt(1) + "-" + nome.charAt(2) + "-" + nome.charAt(3) + "-" + nome.charAt(4));
+        System.out.println("\nFIM DO PROGRMA\n");
 
-         //index/char de uma string
-         String nome = "lucas";
-         System.out.println(nome.charAt(0) + "-" + nome.charAt(1) + "-"
-                 + nome.charAt(2) + "-" + nome.charAt(3) + "-" + nome.charAt(4));
+        //converter valor em string
+        Float f = 10.0f;
+        String.valueOf(f);// 10f e valor float
+        String valorConvertido = String.valueOf(f);
+        System.out.println(valorConvertido);
+        System.out.println("\nFIM DO PROGRMA\n");
 
-         //converter valor em string
-         String.valueOf(10f);// 10f e valor float
+        //Poo vs type primitivos; a prova que java nao e so POO
+        /*
+        Integer n1 = 10;
+        int n2 = 10;
+        OBS:APAGUE O (.) E VEJA AS SUGESTOES.
+        n1.;//possue metidos de funcoes derivadas da POO
+        n2.;//int normal de outras linguagems
+        */
 
-         Float f = 10.0f;
-         String valorConvertido = String.valueOf(f);
-         System.out.println(valorConvertido);
-
-         //Poo vs type primitivos; a prova que java nao e so POO
-         Integer n1 = 10;
-         int n2 = 10;
-         n1.;//possue metidos de funcoes derivadas da POO
-         n2.;//int normal de outras linguagems
-         /**POO     primitivos
-          * Double  double
-          * Flout   flout
-          * Long    long
-          * Integer int
-          * Short   short
-          * Byte    byte
-          * Boolean boolean
-          * Cher    cher
-          * String  nao Tem pois e diferente*/
-     }
- }
+        /**POO     primitivos
+         * Double  double
+         * Flout   flout
+         * Long    long
+         * Integer int
+         * Short   short
+         * Byte    byte
+         * Boolean boolean
+         * Cher    cher
+         * String  nao Tem pois e diferente*/
+    }
+}
