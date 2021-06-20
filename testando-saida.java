4ccb589eb8f536b4d83426210373938891ff575c
Lucas Barbosa Ferreira <55853372+19089910@users.noreply.github.com>
2021-06-20 15:42:36 -0400

    Update testando-saida.java

--- testando-saida.java
+++ testando-saida.java
@@ -1,53 +1,67 @@
-import java.util.Scanner;//biblioteca escan
+ / * tipos de comentaros * /
+         / **
+         * texto
+         * /
+         // ctl + alt + L
+         / * texto * /
+         / * tipo bit
+         (valores quebrados / racionais)
+         Problema 64 (Double d = 10.0241864541485489421057484120468;)
+         Flutuante 32 (Flutuante f = 100,4185465184841231f;)
+         (inteiros)
+         Long 64 (Long l = 10165412021adsfasdfasdfasdfasdfasdfal;)
+         Int 32 (inteiro i = 100;)
+         Curto 16 (Curto S = 10;)
+         Byte 8 (Byte b = 10;)
+         Caractere 2 (caractere c = 'a')
+         Fragmento ? (terterminado por mim)
+         Boleano ? (determinado pelo JVK se for verdadeiro ou falso)
+         * /
+public  class  OlaMundo {                       // ctl + alt + L = catenacao automatica da ide
+    public  static  void  main ( String [] args ) {   // = psvm
+        Sistema . para fora . println ( " olá, mundo! " );    // = sout
 
-public class saida {
-     public static void main(String[] args) {
-         String seila;
 
-         Scanner scan = new Scanner(System.in);//= type variavelName = novo type(entrar no sistema)/ variavelName recebe type(sua funcao)
+        // VARIAVES
+        String nome =  " Lucas " ;
+        Soma inteiro =  10  +  2 ;
 
-         System.out.print("informe um nome: ");
-         seila = scan.nextLine();/** String stringname = scanner.nextLine();
-                                   * Double doubleValor = scanner.nextDoubel();
-                                   * Fluat floatValor = scanner.nextFluat();
-                                   * Short shortValor = scanner.nextShort();
-                                   * Integer intValor = scanner.nextInt();
-                                   * Byte byteValor = scanner.nextByte();
-                                   * Boolean bool = scanner.nextBoolean();
-                                */
-         System.out.println(seila);
-        //teste
-         System.out.print("digite um valor: ");
-         Integer digitaAi = scan.nextInt();
-         System.out.println(digitaAi + 10);
+        Sistema . para fora . println (nome);
+        Sistema . para fora . println (soma);
 
-         //index/char de uma string
-         String nome = "lucas";
-         System.out.println(nome.charAt(0) + "-" + nome.charAt(1) + "-"
-                 + nome.charAt(2) + "-" + nome.charAt(3) + "-" + nome.charAt(4));
 
-         //converter valor em string
-         String.valueOf(10f);// 10f e valor float
+        // USO DE MEMÓRIA DAS VARIAVES
+        Sistema . para fora . println ( " Duplo: Máx: "  +  Duplo . MAX_VALUE  +  " - Duplo: "  +
+                Dobrar . MIN_VALUE );
+        Sistema . para fora . println ( " Float: Max: "  +  Float . MAX_VALUE  +  " - Min: "  +
+                Float . MIN_VALUE );
+        Sistema . para fora . println ( " Long: Max: "  +  Long . MAX_VALUE  +  " - Long: "  +
+                Longo . MIN_VALUE );
+        Sistema . para fora . println ( " Inteiro: Máx: "  +  Inteiro . MAX_VALUE  +  " - Mín: "  +
+                Inteiro . MIN_VALUE );
+        Sistema . para fora . println ( " Curto: Máx: "  +  Curto . MAX_VALUE  +  " - Min: "  +
+                Curto . MIN_VALUE );
+        Sistema . para fora . println ( " Byte: Max: "  +  Byte . MAX_VALUE  +  " - Min: "  +
+                Byte . MIN_VALUE );
 
-         Float f = 10.0f;
-         String valorConvertido = String.valueOf(f);
-         System.out.println(valorConvertido);
 
-         //Poo vs type primitivos; a prova que java nao e so POO
-         Integer n1 = 10;
-         int n2 = 10;
-         n1.;//possue metidos de funcoes derivadas da POO
-         n2.;//int normal de outras linguagems
-         /**POO     primitivos
-          * Double  double
-          * Flout   flout
-          * Long    long
-          * Integer int
-          * Short   short
-          * Byte    byte
-          * Boolean boolean
-          * Cher    cher
-          * String  nao Tem pois e diferente*/
+        // OPERADORES JAVA
+        numero int =  10 ;
+        Sistema . para fora . println ( 2  + numero);
+        Sistema . para fora . println (numero -  8 );
+        Sistema . para fora . println (numero /  5 );
+        Sistema . para fora . println (numero %  3 ); // resto de uma divisão
 
-     }
- }
+        Sistema . para fora . println ( " uso de operadores " );
+        int num =  10 ;
+        num * = 2 ; // = num = num * 2
+        num = num + 1 ; // num = 20 + 1 / num = 21
+        Sistema . para fora . println (num);
+        Sistema . para fora . println ( " incremento e decremento " );
+        // num = 21
+        Sistema . para fora . println (num ++ ); // = / num = num + 1 / num + = 1
+        Sistema . para fora . println (num);
+        Sistema . para fora . println ( - num); // = num-1 = num / 1 = -num
+        // num = 21
+    }
+}
